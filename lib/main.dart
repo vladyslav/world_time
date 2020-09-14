f287c6d7272988f43e5d1a790baaadc6f0b0253d
Vladyslav Vasylenko <vladyslav.vasylenko@dev.pro>
2020-09-14 18:30:04 +0300

    initial upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/pages/chooseLocation.dart';
+import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading.dart';
+
+void main() => runApp(MaterialApp(initialRoute: '/', routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }));
